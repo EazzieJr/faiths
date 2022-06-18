4b0aff908aa4bcb9f985f062621f3609a9bc8ea4
Ganiyu Idris <Golamide27@gmail.com>
2022-06-18 20:01:52 +0100

    Finished with the footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,12 +1,9 @@
 <template>
-  <div class="">
-    <div
-      class="font-kugile col-end md:flex-row md:evenly px-6 md:px-[3.35vw] border-color border-t"
-    >
-      <div
-        class="w-full h-full space-y-7 md:space-y-[3.35vw] pl-6 lg:pl-[3.35vw] pt-16 md:pt-[8.3vw] pb-14 md:pb-[8vw] border-l border-r-0 md:border-r border-color"
-      >
-        <h2 class="">SOCIALS</h2>
+  <footer>
+    <div class="footer-container">
+      <div class="top-left">
+        <h2>SOCIALS</h2>
+
         <a
           v-for="social in socials"
           :key="social.name"
@@ -15,23 +12,20 @@
           data-cursor="-sm"
           data-magnetic
         >
-          <span class="first-letter:capitalize text-xl md:text-[2.13vw]">{{
-            social.name
-          }}</span>
+          <span class="social-name">{{ social.name }}</span>
+
           <span class="w-8 md:min-w-[3.6vw]">
             <img src="svg/arrow.svg" alt="" />
           </span>
         </a>
       </div>
 
-      <div
-        class="block w-full h-full py-12 md:py-0 md:pt-[3vw] center border-l border-t md:border-0 border-color"
-      >
-        <p class="text-xl text-center md:text-[2.13vw] md:leading-[3.5vw]">
+      <div class="connect">
+        <p>
           Connect with me
           <br />
           via mail at <br />
-          Faith.olaniyi@gmail.com
+          <a href="mailto: faith.olaniyi@gmail.com">Faith.olaniyi@gmail.com</a>
         </p>
       </div>
     </div>
@@ -39,7 +33,7 @@
     <div class="copyright">
       <p>Designed AT CENTURY LEAP</p>
     </div>
-  </div>
+  </footer>
 </template>
 
 <script>
@@ -59,6 +53,28 @@ export default {
 </script>
 
 <style lang="postcss" scoped>
+footer {
+  > .footer-container {
+    @apply font-kugile col-end md:flex-row md:evenly px-6 md:px-[3.35vw] border-color border-t;
+
+    > .top-left {
+      @apply w-full h-full space-y-7 md:space-y-[3.35vw] pl-6 lg:pl-[3.35vw] pt-16 md:pt-[8.3vw] pb-14 md:pb-[15vw] border-l border-r-0 md:border-r border-color;
+    }
+  }
+}
+.social-name {
+  @apply first-letter:capitalize text-xl md:text-[2.13vw] md:leading-[3.4vw];
+  
+}
+
+.connect {
+  @apply block w-full h-full py-12 md:py-0 md:pt-[3vw] center border-l border-t md:border-0 border-color;
+
+  p {
+    @apply text-xl text-center md:text-[2.13vw] md:leading-[3.5vw];
+  }
+}
+
 .copyright {
   @apply px-6 border-t start py-8 md:px-[3.35vw] md:pt-[3.8vw] md:pb-[3.2vw] border-color;
 
