99f5899a71a1b6425b3c0344486bf96d54a98ccd
Ganiyu Idris <Golamide27@gmail.com>
2022-06-18 20:49:05 +0100

    Finished styling the certifications

--- components/Certifications.vue
+++ components/Certifications.vue
@@ -1,27 +1,17 @@
 <template>
-  <section
-    class="certifications px-6 md:px-[3.35vw] border-color border-t relative"
-  >
-    <div
-      class="certifications-container border-color border-l pl-6 md:pl-[3.35vw] pt-16 md:pt-[8.3vw] pb-[13.4vw]"
-    >
+  <section class="certifications">
+    <div class="certifications-container">
       <div class="top">
         <h2 class="relative">
           certifications
-          <span
-            class="inline-block absolute top-0 -translate-y-1/2 w-[2rem] lg:w-[4vw] ml-[1vw]"
-          >
-            <Asterisk />
+          <span>
+            <img src="svg/asterisk.svg" alt="" />
           </span>
         </h2>
       </div>
 
-      <div
-        class="bottom mt-10 md:mt-[2.2vw] start-start space-x-[7vw] lg:space-x-[10vw]"
-      >
-        <div
-          class="texts space-y-20 md:space-y-[9.92vw] md:mt-[8.92vw] md:max-w-[42vw]"
-        >
+      <div class="bottom">
+        <div class="texts">
           <div
             v-for="certificate in certificates"
             :key="certificate.id"
@@ -58,10 +48,8 @@
       </div>
     </div>
 
-    <div
-      class="absolute right-[10.81vw] top-[56.3vw] w-10 lg:w-[2.71vw] rotate-45"
-    >
-      <StarIcon />
+    <div class="image">
+      <img src="svg/staricon.svg" alt="" />
     </div>
   </section>
 </template>
@@ -114,4 +102,30 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="postcss" scoped>
+.certifications {
+  @apply px-6 md:px-[3.35vw] border-color border-t relative;
+
+  > .certifications-container {
+    @apply border-color border-l pl-6 md:pl-[3.35vw] pt-16 md:pt-[8.3vw] pb-[13.4vw];
+
+    > .top {
+      span {
+        @apply inline-block absolute top-0 -translate-y-1/2 w-[2rem] lg:w-[4vw] ml-[1vw];
+      }
+    }
+
+    > .bottom {
+      @apply mt-10 md:mt-[2.2vw] start-start space-x-[7vw] lg:space-x-[10vw];
+
+      > .texts {
+        @apply space-y-20 md:space-y-[9.92vw] md:mt-[8.92vw] md:max-w-[42vw];
+      }
+    }
+  }
+
+  > .image {
+    @apply absolute right-[10.81vw] top-[56.3vw] w-10 lg:w-[2.71vw] rotate-45;
+  }
+}
+</style>
