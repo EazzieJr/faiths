d68c62edb8fb784d6f5f7d1b9062bd84f95fbd5b
Ganiyu Idris <Golamide27@gmail.com>
2022-06-19 10:02:12 +0100

    Fixed some margin issue

--- components/ScrollDown.vue
+++ components/ScrollDown.vue
@@ -2,7 +2,7 @@
 <div class="max-w-[24px] md:max-w-[3.35vw] mt-[50vh] md:mt-[50vh]">
   <span class="-rotate-90 text-xs uppercase flex whitespace-nowrap letter-spacing md:text-[1.06vw] md:leading-[1.8vw] tracking-tight md:tracking-[0.3vw]">scroll down</span>
 
-  <img class="rotate-90 mt-[7vh] md:mt-[10vh]" src="svg/arrow.svg" alt="">
+  <img class="rotate-90 mt-[7vh] md:mt-[10vh] mx-auto" src="svg/arrow.svg" alt="">
 </div>
 </template>
 
